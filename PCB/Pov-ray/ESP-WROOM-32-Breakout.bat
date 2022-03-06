088acc81dd1410a6211490badbb3b152530ecc3e
eunbiline98 <aththaareq.mahesa@gmail.com>
2022-03-06 20:01:39 +0700

    Adding file pov-ray

---
+++ PCB/Pov-ray/ESP-WROOM-32-Breakout.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"pvengine.exe" +W1600 +H1200 +A0.3 +FN +L"c:/windows/fonts" +L"C:/EAGLE-7.2.0/ulp/eagle3d/ulp//../povray" +O"C:/Users/Ath Thaareq Mahesa/Documents/Work/Eunbiline98_project/keyless-door-lock-with-bluetooth/ESP-WROOM-32-Breakout.png" /RENDER "C:/Users/Ath Thaareq Mahesa/Documents/Work/Eunbiline98_project/keyless-door-lock-with-bluetooth/ESP-WROOM-32-Breakout.pov" 
